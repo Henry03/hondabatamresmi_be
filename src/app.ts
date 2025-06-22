17bad705555ad7b9c0ae7921aaf6e072ba5e0466
Henry <henryyanggg@gmail.com>
2025-06-22 17:51:22 +0700

    Fixing: move /uploads to /api/v1/uploads

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,6 @@ import { validateJson } from './middlewares/validateJson';
 import { errorHandler } from './middlewares/errorHandler';
 import cors from 'cors';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -20,7 +16,6 @@ app.use(cors({
 app.use(express.json());
 app.use(validateJson);
 
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use('/api/v1', routes);
 
 app.use(errorHandler);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+import express from 'express';
 import { Router } from 'express';
 import carRoutes from './car.routes';
 import authRoutes from './auth.routes'
@@ -6,6 +7,12 @@ import promoRoutes from './promo.routes'
 import commentRoutes from './comment.routes'
 import certificateRoutes from './certificate.routes'
 import carouselRoutes from './carousel.routes'
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const router = Router();
 
@@ -16,5 +23,7 @@ router.use('/promos', promoRoutes);
 router.use('/comments', commentRoutes);
 router.use('/certificates', certificateRoutes);
 router.use('/carousels', carouselRoutes);
+router.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+
 
 export default router;
